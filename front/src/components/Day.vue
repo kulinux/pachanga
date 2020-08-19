bf671be38986a0fb21b7c33cdc9dc22c1a5d5551
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2020-08-19 23:28:22 +0200

    I have status

--- front/src/components/Day.vue
+++ front/src/components/Day.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="day">
-    <p>
+    <div class="day-header">
       <button @click="all">+</button>
-      {{name}}
+      <span>{{name}}</span>
       <button @click="none">-</button>
-    </p>
+    </div>
     <div class="day-hours">
-      <Hour v-for="(hour, index) in hours"
+      <Hour
+        v-for="(hour, index) in hours"
         :hour="hour"
         :key="hour"
         :index="index"
@@ -14,7 +15,8 @@
         @hourClick="hourClick"
         @hourMouseEnter="hourMouseEnter"
         @hourMouseLeave="hourMouseLeave"
-        :selectedOnMouseOver="selectedOnMouseOver" />
+        :selectedOnMouseOver="selectedOnMouseOver"
+      />
     </div>
   </div>
 </template>
@@ -32,9 +34,28 @@ export default {
       required: true,
     },
     hours: {
-        type: Array, 
-        default: () => [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-    }
+      type: Array,
+      default: () => [
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+      ],
+    },
   },
   data() {
     return {
@@ -43,48 +64,58 @@ export default {
       currentIndexSelectedEnter: -1,
       currentIndexSelectedLeave: -1,
       currentDirection: 0,
-      from: {hour: -1, min: -1},
-      to: {hour: -1, to: -1}
-    }
+      from: { hour: -1, min: -1 },
+      to: { hour: -1, to: -1 },
+    };
   },
   methods: {
-    hourClick: function(event) {
-      if(!this.selectedOnMouseOver) {
-        this.from = {hour: event.hour, min: event.min}
+    hourClick: function (event) {
+      if (!this.selectedOnMouseOver) {
+        this.from = { hour: event.hour, min: event.min };
       } else {
-        this.to = {hour: event.hour, min: event.min}
-        this.$emit('rangeAdd', {day: this.name, from: this.from, to: this.to})
+        this.to = { hour: event.hour, min: event.min };
+        this.$emit("rangeAdd", {
+          day: this.name,
+          from: {
+            hour: this.from.hour,
+            min: this.from.min,
+          },
+          to: {
+            hour: this.to.hour,
+            min: this.to.min
+          }
+        });
       }
 
       this.selectedOnMouseOver = !this.selectedOnMouseOver;
       this.$set(this.selected, event.index, true);
     },
-    hourMouseEnter: function(event) {
-      if(this.selectedOnMouseOver) {
+    hourMouseEnter: function (event) {
+      if (this.selectedOnMouseOver) {
         this.$set(this.selected, event.index, true);
       }
       this.currentIndexSelectedEnter = event.index;
     },
-    hourMouseLeave: function(event) {
+    hourMouseLeave: function (event) {
       this.currentIndexSelectedLeave = event.index;
     },
-    all: function() {
-      this.hours.forEach((value, index) => 
+    all: function () {
+      this.hours.forEach((value, index) =>
         this.$set(this.selected, index, true)
-      )
-      this.$emit('rangeAll', {
-        day: this.name
-      })
+      );
+      this.$emit("rangeAll", {
+        day: this.name,
+      });
     },
-    none: function() {
-      this.hours.forEach((value, index) => 
+    none: function () {
+      this.hours.forEach((value, index) =>
         this.$set(this.selected, index, false)
-      )
-      this.$emit('rangeClear', {
-        day: this.name
-      })
-    }
-  }
+      );
+      this.$emit("rangeClear", {
+        day: this.name,
+      });
+    },
+  },
 };
 </script>
 
@@ -93,5 +124,23 @@ export default {
   display: flex;
   flex-direction: column;
   border-style: solid;
+}
+
+.day-header {
+  margin: 0.5em;
+  display: flex;
+}
+.day-header > * {
+  flex-grow: 1;
+}
+
+.day-header > * + * {
+  margin-left: 0.5em;
+}
+
+@media (max-width: 560px) {
+  .day-header {
+    flex-direction: column;
+  }
 }
 </style>
--- front/src/components/Week.vue
+++ front/src/components/Week.vue
@@ -2,13 +2,13 @@
   <div>
     <p>Soy week</p>
     <div class="week">
-      <Day name="Monday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear"/>
-      <Day name="Tuesday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear"/>
-      <Day name="Wednesday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear"/>
-      <Day name="Thursday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear"/>
-      <Day name="Friday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear"/>
-      <Day name="Saturday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear"/>
-      <Day name="Sunday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear"/>
+      <Day name="Monday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear" />
+      <Day name="Tuesday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear" />
+      <Day name="Wednesday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear" />
+      <Day name="Thursday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear" />
+      <Day name="Friday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear" />
+      <Day name="Saturday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear" />
+      <Day name="Sunday" @rangeAdd="rangeAdd" @rangeAll="rangeAll" @rangeClear="rangeClear" />
     </div>
   </div>
 </template>
@@ -21,17 +21,32 @@ export default {
   components: {
     Day,
   },
+  data() {
+    return {
+      schedule: {},
+    };
+  },
   methods: {
-      rangeAdd: function(ev) {
-          console.log('RangeAdd', ev);
-      },
-      rangeClear: function(ev) {
-          console.log('RangeClear', ev);
-      },
-      rangeAll: function(ev) {
-          console.log('RangeAll', ev);
+    rangeAdd: function (ev) {
+      console.log("RangeAdd", ev);
+      let newItem = { from: ev.from, to: ev.to };
+      if (!this.schedule[ev.day]) {
+        this.schedule[ev.day] = [newItem];
+      } else {
+        this.schedule[ev.day] = this.schedule[ev.day].concat(newItem);
       }
-  }
+    },
+    rangeClear: function (ev) {
+      delete this.schedule[ev.day];
+      console.log("RangeClear", ev);
+    },
+    rangeAll: function (ev) {
+      this.schedule[ev.day] = [
+        { from: { hour: 0, min: 0 }, to: { hour: 23, min: 59 } },
+      ];
+      console.log("RangeAll", ev);
+    },
+  },
 };
 </script>
 
